ae5a4980867d296684b977a8c4754fd7aee625cd
FransysDrake <fransysdrake@mail.ru>
2016-07-15 11:58:20 +0600

    Initial commit

---
+++ src/main/java/java/com/at/Entity/BaseEntity.java
@@ -0,0 +1,19 @@
+package java.com.at.Entity;
+
+/**
+ * Created by Artur on 15.07.2016.
+ */
+public class BaseEntity {
+private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public BaseEntity() {
+    }
+}
---
+++ src/main/java/java/com/at/Entity/MicrowaveOven.java
@@ -0,0 +1,7 @@
+package java.com.at.Entity;
+
+/**
+ * Created by Artur on 15.07.2016.
+ */
+public class MicrowaveOven {
+}
